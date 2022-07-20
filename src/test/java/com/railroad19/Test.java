b5f6773d3bd78b6fe75ef337c55237ee59fbf068
Stephen Ball <ball@railroad19.com>
2022-07-20 19:12:59 -0400

    Working test

---
+++ src/test/java/com/railroad19/Test.java
@@ -0,0 +1,13 @@
+package com.railroad19;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class Test {
+
+  @org.junit.jupiter.api.Test
+  public void sanityCheck() {
+
+    System.out.println("This works!");
+    assertTrue(true);
+  }
+}
